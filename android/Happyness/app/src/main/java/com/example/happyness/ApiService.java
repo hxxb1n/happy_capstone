722ad78f95f6a057f7f327aa2bd54e16cf706525
snowman4147 <rag8994vcs2@gmail.com>
2024-05-09 15:17:23 +0900

    first

---
+++ android/Happyness/app/src/main/java/com/example/happyness/ApiService.java
@@ -0,0 +1,10 @@
+package com.example.happyness;
+import retrofit2.Call;
+import retrofit2.http.Body;
+import retrofit2.http.POST;
+
+public interface ApiService {
+    @POST("/test/post-method")
+    Call<Void> createUser(@Body User user);
+}
+
